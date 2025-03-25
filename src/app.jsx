7c3f58adda43ce2fc28fe223d36ac39a469eda3a
Tobywhunt <tobywhunt@gmail.com>
2025-03-24 23:29:03 -0600

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { AuthState } from './login/authState';
+import './app.css';
+
+function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
+
+  // Store the scores in the state
+  const [scores, setScores] = React.useState({
+    green: '',
+    red: '',
+    blue: '',
+    purple: '',
+    orange: '',
+  });
+
+  return (
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              FrostySnippers
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Login
+                </NavLink>
+              </li>
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='play'>
+                    Play
+                  </NavLink>
+                </li>
+              )}
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='scores'>
+                    Messages
+                  </NavLink>
+                </li>
+              )}
+            </menu>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            }
+            exact
+          />
+          <Route 
+            path='/play' 
+            element={<Play userName={userName} setScores={setScores} />} 
+          />
+          {/* Pass the scores state to the Scores page */}
+          <Route 
+            path='/scores' 
+            element={<Scores scores={scores} />} 
+          />
+        </Routes>
+
+        <footer className='bg-dark text-dark text-muted'>
+          <div className='container-fluid'>
+            <a className='text-reset' href='https://github.com/Tobywarrenhunt/StartUp-260'>
+              Github
+            </a>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
+
+export default App;
